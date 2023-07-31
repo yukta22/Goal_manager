2e28003da78cdd16113a249c9882cdd8c0306e1f
yukta22 <60733741+yukta22@users.noreply.github.com>
2023-07-31 11:42:27 +0530

    deleted

--- Goal_Manager/back_end/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createUser, getUser } from "../controllers/user.controllers.js";
-
-export const userRoute = express.Router();
-
-userRoute.post("/register/user", createUser);
-userRoute.post("/login/user", getUser);
